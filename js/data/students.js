4916f71e92bba15ee4269633dc494d210d2e0ef2
chris <chris@MGA.LOCAL>
2024-02-21 11:01:06 -0400

    Student updates

--- js/data/students.js
+++ js/data/students.js
@@ -7,11 +7,13 @@ export const students = [
         name: 'Echeandia, Manuel',
         class: 'WMAD Class of 2024',
         website: 'https://manuelecheandia.com/'
-    }, {
-        name: 'Cserkuti, Catie',
-        class: 'WMAD Class of 2024',
-        website: 'https://www.linkedin.com/in/catie-cserkuti/'
-    }, {
+    }, 
+    // {
+    //     name: 'Cserkuti, Catie',
+    //     class: 'WMAD Class of 2024',
+    //     website: 'https://www.linkedin.com/in/catie-cserkuti/'
+    // }, 
+    {
         name: 'Dorfman, Anastasia',
         class: 'WMAD Class of 2024',
         website: 'https://anastasiadorfman.com/'
