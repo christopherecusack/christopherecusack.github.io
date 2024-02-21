7dd7ab7373a324c6b32b8da7c8f740145d4fab73
chris <chris@MGA.LOCAL>
2024-02-21 08:26:38 -0400

    Student additions

--- js/data/students.js
+++ js/data/students.js
@@ -3,5 +3,21 @@ export const students = [
         name: 'Parasio, Herbert',
         class: 'GEX Class of 2024',
         website: 'https://herbertph.com/'
+    }, {
+        name: 'Manuel Echeandia',
+        class: 'WMAD Class of 2024',
+        website: 'https://manuelecheandia.com/'
+    }, {
+        name: 'Manuel Echeandia',
+        class: 'WMAD Class of 2024',
+        website: 'https://manuelecheandia.com/'
+    }, {
+        name: 'Catie Cserkuti',
+        class: 'WMAD Class of 2024',
+        website: 'https://www.linkedin.com/in/catie-cserkuti/'
+    }, {
+        name: 'Anastasia Dorfman',
+        class: 'WMAD Class of 2024',
+        website: 'https://anastasiadorfman.com/'
     }
-].sort((a, b) => a.name > b.name ? 1: -1);
+].sort((a, b) => a.name > b.name ? 1 : -1);
