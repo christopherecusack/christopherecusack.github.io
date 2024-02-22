05f1ca9e00d897ab59ae16c06c058b233932dba3
chris <chris@MGA.local>
2024-02-22 15:52:58 -0400

    Student updates

--- js/data/students.js
+++ js/data/students.js
@@ -1,27 +1,27 @@
 export const students = [
     {
         name: 'Parasio, Herbert',
-        class: 'GEX Class of 2024',
+        class: 'Gaming Experience Class of 2024',
         website: 'https://herbertph.com/'
     }, {
         name: 'Echeandia, Manuel',
-        class: 'WMAD Class of 2024',
+        class: 'Web &amp; Mobile Application Development Class of 2024',
         website: 'https://manuelecheandia.com/'
     }, 
     {
         name: 'Saenz Montano, Marcela',
-        class: 'WMAD Class of 2024',
+        class: 'Web &amp; Mobile Application Development Class of 2024',
         website: 'https://marcelasaenz.com/'
     }, 
     {
         name: 'Dorfman, Anastasia',
-        class: 'WMAD Class of 2024',
+        class: 'Web &amp; Mobile Application Development Class of 2024',
         website: 'https://anastasiadorfman.com/'
     }
     , 
     {
         name: 'Budd, Taylor',
-        class: 'WMAD Class of 2023',
+        class: 'Web &amp; Mobile Application Development Class of 2023',
         website: 'https://taylorbudd.com/'
     }
 ].sort((a, b) => a.name > b.name ? 1 : -1);
