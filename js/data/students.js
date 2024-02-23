88ee1487fffad4904ddc30178ad4b6a3168b5a82
chris <chris@MGA.LOCAL>
2024-02-22 22:15:47 -0400

    Student updates

--- js/data/students.js
+++ js/data/students.js
@@ -7,21 +7,21 @@ export const students = [
         name: 'Echeandia, Manuel',
         class: 'Web &amp; Mobile Application Development Class of 2024',
         website: 'https://manuelecheandia.com/'
-    }, 
-    {
+    }, {
         name: 'Saenz Montano, Marcela',
         class: 'Web &amp; Mobile Application Development Class of 2024',
         website: 'https://marcelasaenz.com/'
-    }, 
-    {
+    }, {
         name: 'Dorfman, Anastasia',
         class: 'Web &amp; Mobile Application Development Class of 2024',
         website: 'https://anastasiadorfman.com/'
-    }
-    , 
-    {
+    }, {
         name: 'Budd, Taylor',
         class: 'Web &amp; Mobile Application Development Class of 2023',
         website: 'https://taylorbudd.com/'
+    }, {
+        name: 'Suarez, Manuel',
+        class: 'Gaming Experience Class of 2024',
+        website: 'https://manuelsuarez3d.com/'
     }
 ].sort((a, b) => a.name > b.name ? 1 : -1);
