feee177fa55ebed22c1172acee927032a6e3c0ad
chris <chris@MGA.LOCAL>
2025-03-03 15:43:44 -0400

    Updated student data

--- js/data/students.js
+++ js/data/students.js
@@ -8,18 +8,6 @@ export const students = [
         class: 'Web &amp; Mobile Application Development Class of 2024',
         website: 'https://manuelecheandia.com/'
     }, {
-        name: 'Saenz Montano, Marcela',
-        class: 'Web &amp; Mobile Application Development Class of 2024',
-        website: 'https://marcelasaenz.com/'
-    }, {
-        name: 'Rahman, Mahfuzur',
-        class: 'Web &amp; Mobile Application Development Class of 2024',
-        website: 'https://mahfuzurr.com/'
-    }, {
-        name: 'Quintana, Pablo',
-        class: 'Web &amp; Mobile Application Development Class of 2024',
-        website: 'https://pabloquintana.info/'
-    }, {
         name: 'Dorfman, Anastasia',
         class: 'Web &amp; Mobile Application Development Class of 2024',
         website: 'https://anastasiadorfman.com/'
@@ -27,10 +15,6 @@ export const students = [
         name: 'Budd, Taylor',
         class: 'Web &amp; Mobile Application Development Class of 2023',
         website: 'https://taylorbudd.com/'
-    }, {
-        name: 'Suarez, Manuel',
-        class: 'Gaming Experience Class of 2024',
-        website: 'https://manuelsuarez3d.com/'
     }, {
         name: 'Portella, Diego',
         class: 'Gaming Experience Class of 2024',
